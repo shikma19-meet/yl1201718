549575c8b6fc99c95c3671aff96ad97e9dbe102d
adan19-meet <adan19@meet.mit.edu>
2017-11-01 19:26:24 +0200

    lab3

---
+++ lab3/TurtleShapes.py
@@ -0,0 +1,13 @@
+import turtle
+for i in range (5):
+	turtle.left(145)
+	turtle.forward(10)
+
+
+turtle.register_shape("hmmmm", ((50,50),(100,50),(50,0),(25,-25),(5S)) )
+	
+
+turtle.mainloop()
+
+
+
